46fac42f8a14d170b0df20db00b4fb2deceb70d4
Bryan Bugyi <bryanbugyi34@gmail.com>
2018-03-17 17:45:35 -0400

    Update: qutebrowser

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -10,14 +10,18 @@ config.load_autoconfig()
 
 # ----- Dictionary Values
 c.url.searchengines['DEFAULT'] = 'http://www.google.com/search?q={}'
+c.url.searchengines['al'] = 'http://www.google.com/search?q=arch+linux+{}'
 c.url.searchengines['gg'] = 'https://www.google.com/search?q=site%3Agithub.com+{}'
 c.url.searchengines['ggg'] = 'https://github.com/bbugyi200/{}'
+c.url.searchengines['waf'] = 'https://waffle.io/bbugyi200/{}'
 c.url.searchengines['li'] = 'https://www.google.com/search?q=site%3Alinkedin.com+{}'
 c.url.searchengines['red'] = 'https://www.google.com/search?q=site%3Areddit.com+{}'
 c.url.searchengines['a'] = 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords={}'
 c.url.searchengines['w'] = 'https://www.wikipedia.org/w/index.php?title=Special:Search&search={}'
 c.url.searchengines['yt'] = 'https://www.youtube.com/results?search_query={}'
 c.url.searchengines['img'] = 'https://www.google.com/search?tbm=isch&q={}'
+c.url.searchengines['t'] = 'http://www.thesaurus.com/browse/{}'
+c.url.searchengines['d'] = 'http://www.dictionary.com/browse/{}'
 
 
 # ----- Bindings
@@ -25,15 +29,21 @@ def bind(key, *commands, mode='normal'):
     config.bind(key, ' ;; '.join(commands), mode=mode)
 
 
-bind('<Ctrl-r>', 'restart')
-bind('gi', 'hint inputs')
-bind(',e', 'click-element id lst-ib', 'later 50 open-editor')
-bind(',t', 'config-cycle tabs.position left top')
-
+# qute-pass
 bind('<Ctrl-i>', 'spawn -d qute-pass-add {url}', mode='insert')
 bind('<Ctrl-p>', 'spawn --userscript qute-pass', mode='insert')
-bind('<Ctrl-Alt-p>', 'spawn --userscript qute-pass --username-only', mode='insert')
-bind('<Ctrl-Shift-p>', 'spawn --userscript qute-pass --password-only', mode='insert')
+bind('<Ctrl-Alt-u>', 'spawn --userscript qute-pass --username-only', mode='insert')
+bind('<Ctrl-Alt-p>', 'spawn --userscript qute-pass --password-only', mode='insert')
+
+# misc
+bind('<Ctrl-r>', 'restart')
+bind('<Ctrl-t>', 'spawn --userscript taskadd +read')
+bind('<Ctrl-l>', 'edit-url')
+bind('gi', 'hint inputs')
+bind(',e', 'scroll-to-perc 0', 'later 25 hint inputs -m number',
+     'later 50 spawn xdotool key 0', 'later 75 open-editor')
+bind(',t', 'config-cycle tabs.position left top')
+bind(',rss', 'spawn --userscript openfeeds')
 
 
 # ----- Load Yaml Config
--- .config/qutebrowser/editor.vimrc
+++ .config/qutebrowser/editor.vimrc
@@ -1,2 +1,3 @@
+" let g:buftabline_show = 0
 nnoremap <CR> :wq<CR>
 imap <CR> <Esc><CR>
