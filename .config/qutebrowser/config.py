e3aebe6391ebe491812f34fd9cf927a98cb798e9
Bryan Bugyi <bryanbugyi34@gmail.com>
2018-03-18 05:51:50 -0400

    Update: qutebrowser confs

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -10,18 +10,18 @@ config.load_autoconfig()
 
 # ----- Dictionary Values
 c.url.searchengines['DEFAULT'] = 'http://www.google.com/search?q={}'
+c.url.searchengines['a'] = 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords={}'
 c.url.searchengines['al'] = 'http://www.google.com/search?q=arch+linux+{}'
+c.url.searchengines['d'] = 'http://www.dictionary.com/browse/{}'
 c.url.searchengines['gg'] = 'https://www.google.com/search?q=site%3Agithub.com+{}'
 c.url.searchengines['ggg'] = 'https://github.com/bbugyi200/{}'
-c.url.searchengines['waf'] = 'https://waffle.io/bbugyi200/{}'
+c.url.searchengines['img'] = 'https://www.google.com/search?tbm=isch&q={}'
 c.url.searchengines['li'] = 'https://www.google.com/search?q=site%3Alinkedin.com+{}'
 c.url.searchengines['red'] = 'https://www.google.com/search?q=site%3Areddit.com+{}'
-c.url.searchengines['a'] = 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords={}'
-c.url.searchengines['w'] = 'https://www.wikipedia.org/w/index.php?title=Special:Search&search={}'
-c.url.searchengines['yt'] = 'https://www.youtube.com/results?search_query={}'
-c.url.searchengines['img'] = 'https://www.google.com/search?tbm=isch&q={}'
 c.url.searchengines['t'] = 'http://www.thesaurus.com/browse/{}'
-c.url.searchengines['d'] = 'http://www.dictionary.com/browse/{}'
+c.url.searchengines['w'] = 'https://www.wikipedia.org/w/index.php?title=Special:Search&search={}'
+c.url.searchengines['waf'] = 'https://waffle.io/bbugyi200/{}'
+c.url.searchengines['yt'] = 'https://www.youtube.com/results?search_query={}'
 
 
 # ----- Bindings
@@ -29,21 +29,27 @@ def bind(key, *commands, mode='normal'):
     config.bind(key, ' ;; '.join(commands), mode=mode)
 
 
-# qute-pass
+# >>> INSERT
 bind('<Ctrl-i>', 'spawn -d qute-pass-add {url}', mode='insert')
 bind('<Ctrl-p>', 'spawn --userscript qute-pass', mode='insert')
 bind('<Ctrl-Alt-u>', 'spawn --userscript qute-pass --username-only', mode='insert')
 bind('<Ctrl-Alt-p>', 'spawn --userscript qute-pass --password-only', mode='insert')
 
-# misc
+# >>> COMMAND
+bind('<Ctrl-f>', 'edit-command', mode='command')
+
+# >>> NORMAL
+# Ctrl
 bind('<Ctrl-r>', 'restart')
 bind('<Ctrl-t>', 'spawn --userscript taskadd +read')
 bind('<Ctrl-l>', 'edit-url')
-bind('gi', 'hint inputs')
+# Leader (,)
 bind(',e', 'scroll-to-perc 0', 'later 25 hint inputs -m number',
      'later 50 spawn xdotool key 0', 'later 75 open-editor')
 bind(',t', 'config-cycle tabs.position left top')
 bind(',rss', 'spawn --userscript openfeeds')
+# Miscellaneous
+bind('gi', 'hint inputs')
 
 
 # ----- Load Yaml Config
