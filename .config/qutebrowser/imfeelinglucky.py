c1b656186452905bc8f172a16b2c05eef748554e
Bryan Bugyi <bryanbugyi34@gmail.com>
2018-04-29 00:47:13 -0400

    Mod: optimized imfeelinglucky

--- .config/qutebrowser/imfeelinglucky.py
+++ .config/qutebrowser/imfeelinglucky.py
@@ -7,6 +7,7 @@
 ##########################################################
 
 import argparse
+import re
 import requests
 from bs4 import BeautifulSoup
 
@@ -25,38 +26,19 @@ def fetch_results(query):
     response = requests.get(google_url, headers=USER_AGENT)
     response.raise_for_status()
 
-    return query, response.text
-
-
-def parse_results(html, keyword):
-    soup = BeautifulSoup(html, 'html.parser')
-
-    found_results = []
-    rank = 1
-    result_block = soup.find_all('div', attrs={'class': 'g'})
-    for result in result_block:
-
-        link = result.find('a', href=True)
-        title = result.find('h3', attrs={'class': 'r'})
-        description = result.find('span', attrs={'class': 'st'})
-        if link and title:
-            link = link['href']
-            title = title.get_text()
-            if description:
-                description = description.get_text()
-            if link != '#':
-                found_results.append({'keyword': keyword, 'rank': rank, 'title': title, 'description': description, 'link': link})
-                rank += 1
-    return found_results
+    return response.text
 
 
 def getTopLink(query):
-    try:
-        keyword, html = fetch_results(query)
-        results = parse_results(html, keyword)
-        return results[0]['link']
-    except IndexError as e:
-        return 'https://www.google.com/search?q={}'.format(escape_url(query))
+    html = fetch_results(query)
+    soup = BeautifulSoup(html, 'html.parser')
+    result_block = soup.find_all('div', attrs={'class': 'g'})
+    for result in result_block:
+        link = result.find('a', href=True)
+        if link and link != '#' and re.match('^http[s]?://', link['href']):
+            return link['href']
+
+    return 'https://www.google.com/search?q={}'.format(escape_url(query))
 
 
 if __name__ == "__main__":
