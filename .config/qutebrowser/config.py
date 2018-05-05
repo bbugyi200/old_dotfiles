e6946ed068e92984dfac104cbb2edb273015dadd
Bryan Bugyi <bryanbugyi34@gmail.com>
2018-05-05 19:29:10 -0400

    Mod: initialize searchengines dictionary instead of assigning each key

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -18,50 +18,52 @@ bang_pttrn = bang_fmt.format(''.join(['(?!{})'.format(w) for w in excluded_bangs
                              '|'.join(included_bangs))
 
 # ----- Search Engines
-c.url.searchengines['DEFAULT'] = SE.URL(SE.static.google('{}'),
-                                        SE.static.duckduckgo('{}'),
-                                        SE.static.duckduckgo('!{}'),
-                                        SE.LuckyQuery.url('{}'),
-                                        patterns=('^%21', bang_pttrn, SE.LuckyQuery.pattern),
-                                        filters=(None, None, SE.LuckyQuery.filter))
-c.url.searchengines['ep'] = SE.URL(SE.static.google('{} episodes'),
-                                   SE.static.google('Season {} episodes'),
-                                   patterns=SE.OneIntQuery.pattern)
-c.url.searchengines['al'] = SE.static.google('arch linux {}')
-c.url.searchengines['gh'] = SE.URL(SE.static.google('{} site:github.com'),
-                                   SE.LuckyQuery.url('{} site:github.com'),
-                                   'https://github.com/bbugyi200/{}',
-                                   patterns=(SE.LuckyQuery.pattern, '^%40'),
-                                   filters=(SE.LuckyQuery.filter, lambda x: x.replace('%40', '')))
-c.url.searchengines['ghi'] = SE.URL('https://github.com/bbugyi200/{}/issues',
-                                    'https://github.com/bbugyi200/scripts/issues/{}',
-                                    'https://github.com/bbugyi200/{1}/issues/{0}',
-                                    SE.LuckyQuery.url('{} site:github.com', end='issues?&q=is%3Aissue+{}'),
-                                    SE.LuckyQuery.url('{} site:github.com', end='issues'),
-                                    patterns=('^[0-9]+$',
-                                              SE.OneIntQuery.pattern,
-                                              '{}{}'.format(SE.LuckyQuery.pattern, r'([A-z]|%20)+%3F'),
-                                              SE.LuckyQuery.pattern),
-                                    filters=(None,
-                                             SE.OneIntQuery.filter,
-                                             lambda x: re.split(r'%20%3F', SE.LuckyQuery.filter(x), maxsplit=1),
-                                             SE.LuckyQuery.filter))
-c.url.searchengines['li'] = SE.static.google('site:linkedin.com {}')
-c.url.searchengines['r'] = SE.URL(SE.static.google('{} site:reddit.com'),
-                                  SE.LuckyQuery.url('{} site:reddit.com'),
-                                  patterns=SE.LuckyQuery.pattern,
-                                  filters=SE.LuckyQuery.filter)
-c.url.searchengines['waf'] = 'https://waffle.io/bbugyi200/{}'
-c.url.searchengines['lib'] = 'http://libgen.io/search.php?req={}'
-c.url.searchengines['pir'] = SE.URL('https://thepiratebay.org/search/{}',
-                                    'https://thepiratebay.org/search/{2} S{0:02d}E{1:02d}',
-                                    patterns=SE.TwoIntQuery.pattern,
-                                    filters=SE.TwoIntQuery.filter)
-c.url.searchengines['sub'] = SE.URL(SE.static.google('{} inurl:english site:subscene.com'),
-                                    SE.LuckyQuery.url('{0} inurl:english site:subscene.com'),
-                                    SE.LuckyQuery.url('{2} S{0:02d}E{1:02d} inurl:english site:subscene.com'),
-                                    patterns=(SE.LuckyQuery.pattern, SE.TwoIntQuery.pattern),
-                                    filters=(SE.LuckyQuery.filter, SE.TwoIntQuery.filter))
+c.url.searchengines = {
+    'DEFAULT': SE.URL(SE.static.google('{}'),
+                      SE.static.duckduckgo('{}'),
+                      SE.static.duckduckgo('!{}'),
+                      SE.LuckyQuery.url('{}'),
+                      patterns=('^%21', bang_pttrn, SE.LuckyQuery.pattern),
+                      filters=(None, None, SE.LuckyQuery.filter)),
+    'ep': SE.URL(SE.static.google('{} episodes'),
+                 SE.static.google('Season {} episodes'),
+                 patterns=SE.OneIntQuery.pattern),
+    'al': SE.static.google('arch linux {}'),
+    'gh': SE.URL(SE.static.google('{} site:github.com'),
+                 SE.LuckyQuery.url('{} site:github.com'),
+                 'https://github.com/bbugyi200/{}',
+                 patterns=(SE.LuckyQuery.pattern, '^%40'),
+                 filters=(SE.LuckyQuery.filter, lambda x: x.replace('%40', ''))),
+    'ghi': SE.URL('https://github.com/bbugyi200/{}/issues',
+                  'https://github.com/bbugyi200/scripts/issues/{}',
+                  'https://github.com/bbugyi200/{1}/issues/{0}',
+                  SE.LuckyQuery.url('{} site:github.com', end='issues?&q=is%3Aissue+{}'),
+                  SE.LuckyQuery.url('{} site:github.com', end='issues'),
+                  patterns=('^[0-9]+$',
+                            SE.OneIntQuery.pattern,
+                            '{}{}'.format(SE.LuckyQuery.pattern, r'([A-z]|%20)+%3F'),
+                            SE.LuckyQuery.pattern),
+                  filters=(None,
+                           SE.OneIntQuery.filter,
+                           lambda x: re.split(r'%20%3F', SE.LuckyQuery.filter(x), maxsplit=1),
+                           SE.LuckyQuery.filter)),
+    'li': SE.static.google('site:linkedin.com {}'),
+    'r': SE.URL(SE.static.google('{} site:reddit.com'),
+                SE.LuckyQuery.url('{} site:reddit.com'),
+                patterns=SE.LuckyQuery.pattern,
+                filters=SE.LuckyQuery.filter),
+    'waf':'https://waffle.io/bbugyi200/{}',
+    'lib':'http://libgen.io/search.php?req={}',
+    'pir': SE.URL('https://thepiratebay.org/search/{}',
+                  'https://thepiratebay.org/search/{2} S{0:02d}E{1:02d}',
+                  patterns=SE.TwoIntQuery.pattern,
+                  filters=SE.TwoIntQuery.filter),
+    'sub': SE.URL(SE.static.google('{} inurl:english site:subscene.com'),
+                  SE.LuckyQuery.url('{0} inurl:english site:subscene.com'),
+                  SE.LuckyQuery.url('{2} S{0:02d}E{1:02d} inurl:english site:subscene.com'),
+                  patterns=(SE.LuckyQuery.pattern, SE.TwoIntQuery.pattern),
+                  filters=(SE.LuckyQuery.filter, SE.TwoIntQuery.filter))
+}
 
 # ----- Aliases
 c.aliases['mpv'] = 'spawn --userscript view_in_mpv {url}'
@@ -90,16 +92,21 @@ bind('<Ctrl-f>', 'edit-command', mode='command')
 
 # >>> NORMAL
 # Unbinds
-unbind('b'); unbind('B')  # Be consistent. Use :open to launch quickmarks.
-unbind('d'); unbind('D')
-unbind('gd'); unbind('ad')
-unbind('co')
-unbind('M')
+unbound_keys = ['b', 'B', 'd', 'D', 'gd', 'ad', 'co', 'M']
+for key in unbound_keys:
+    unbind(key)
+# Youtube
+bind('Y', 'spawn ytcast {url}')
+bind(';Y', 'hint links spawn ytcast {hint-url}')
+bind('m', 'hint links spawn umpv {hint-url}')
+bind('M', 'hint links spawn umpv --append {hint-url}')
 # Bookmarks / Quickmarks / Marks
 bind('sq', 'quickmark-save')
 bind('sb', 'bookmark-add')
 bind('dq', 'quickmark-del')
 bind('db', 'bookmark-del')
+bind('a', ':set-cmd-text -s :quickmark-load')
+bind('A', ':set-cmd-text -s :quickmark-load -t')
 # <Ctrl-?>
 bind('<Ctrl-r>', 'restart')
 bind('<Ctrl-t>', 'spawn --userscript taskadd tags:inbox')
@@ -110,6 +117,8 @@ bind(',t', 'config-cycle tabs.position left top')
 bind(',rss', 'spawn --userscript openfeeds')
 bind(',q', 'set-cmd-text :', 'run-with-count 2 command-history-prev', 'edit-command --run')
 # Miscellaneous
+bind('b', 'set-cmd-text -s :buffer')
+bind('gi', 'hint inputs')
 bind('tg', 'set-cmd-text -s :tab-give')
 bind('tt', 'set-cmd-text -s :tab-take')
 bind('x', 'tab-close')
@@ -117,12 +126,8 @@ bind('X', 'tab-close -o')
 bind('D', 'download')
 bind('dd', 'download-cancel')
 bind('to', 'tab-only')
-bind('m', 'hint links spawn umpv {hint-url}')
-bind('M', 'hint links spawn umpv --append {hint-url}')
-bind(';M', 'hint --rapid links spawn umpv --append {hint-url}')
 bind('p', 'open -- {clipboard}')
 bind('P', 'open -t -- {clipboard}')
-bind('ch', 'search')  # Clears Highlighting
 
 # ----- Load Yaml Config
 with (config.configdir / 'config.yml').open() as f:
