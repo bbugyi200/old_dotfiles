9e05f80f5299adba3e52ba7f845b779aa4325c84
Bryan Bugyi <bryanbugyi34@gmail.com>
2018-06-28 19:26:12 -0400

    Test: add test for get_tomorrow

--- .task/hooks/tests/test_dates.py
+++ .task/hooks/tests/test_dates.py
@@ -8,17 +8,35 @@ import pytest
 import dates
 
 
-def test_get_today(shell_date_str):
+def test_get_today(today_shell_date):
     today = dates.get_today()
-    assert today == shell_date_str
+    assert today == today_shell_date
 
 
-def test_get_today_dt(shell_date_str):
+def test_get_today_dt(today_shell_date):
     today_dt = dates.get_today_dt()
-    assert today_dt.strftime(dates.date_fmt) == shell_date_str
+    assert today_dt.strftime(dates.date_fmt) == today_shell_date
+
+
+def test_get_tomorrow(tomorrow_shell_date):
+    tomorrow = dates.get_tomorrow()
+    assert tomorrow == tomorrow_shell_date
 
 
 @pytest.fixture
-def shell_date_str():
-    out = sp.check_output(['date', '+{}'.format(dates.date_fmt.split('T')[0])])
+def today_shell_date():
+    return shell_date()
+
+
+@pytest.fixture
+def tomorrow_shell_date():
+    return shell_date(date='tomorrow')
+
+
+def shell_date(date='today'):
+    cmd_list = ['date']
+    cmd_list.extend(['--date', date])
+    cmd_list.extend(['+{}'.format(dates.date_fmt.split('T')[0])])
+
+    out = sp.check_output(cmd_list)
     return out.decode().strip() + 'T100000Z'
