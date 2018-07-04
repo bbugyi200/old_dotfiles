fab571fb4c0b337d94e08a7aca1b8931a61872c7
Bryan Bugyi <bryanbugyi34@gmail.com>
2018-07-04 11:25:36 -0400

    Ref: organized shell aliases alphabetically

--- extras/aliases.sh
+++ extras/aliases.sh
@@ -1,106 +1,80 @@
 source /home/bryan/Dropbox/dotfiles/home/extras/GTD.sh
 
-# ---------- MISCELLANEOUS ----------
-alias actvenv='source venv/bin/activate'
-alias alg='alias | grep -e'
-auto() { nohup autodemo "$@" &> /dev/null & disown && clear; }
-alias cppinit='cinit ++'
-alias delshots='confirm "find /home/bryan/Dropbox/var/aphrodite-motion -name \"*$(date +%Y%m%d)*\" -delete"'
-alias lpass-login='lpass login bryanbugyi34@gmail.com'
-alias mirror='xrandr --output DVI-I-1-1 --auto --same-as LVDS1'
-# Create and CD to New Directory
-mkcd() { mkdir -p $1 && cd $1; }
-alias gmutt="neomutt -e 'source ~/.mutt/hooks/bryanbugyi34.gmail'"
-alias rmutt='sudo neomutt -f /var/spool/mail/root'
-alias mutt="neomutt"
-alias nomirror='xrandr --output DVI-I-1-1 --auto --right-of LVDS1'
-alias psgrep='ps -aux | grep -E'
-# Prints the value of a variable
-pvar () { set | grep -i -e "^$1"; }
-alias rg='ranger'
-alias snapshots='find /home/bryan/Dropbox/var/aphrodite-motion -name "*$(date +%Y%m%d)*" | sort | xargs imv && delshots'
-# Sleep, then run the last command
-ss () { tmux send-keys "sleep 1.5 && !-2" "Enter"; }
-alias vplug="vim -c ':PluginInstall'"
-alias vcplug="vim -c ':PluginClean'"
-alias w='which'
-
-# ---------- DROPBOX ----------
-alias dst='dropbox-cli status'
-alias dfs='dropbox-cli filestatus'
-alias watdst='watch -n 5 dropbox-cli status'
-alias dstart='dropbox-cli start'
-alias dstop='dropbox-cli stop'
-
-# ---------- Vim ----------
-alias lim='vim -c "normal \`0" -c "bdelete 1"'
-alias onething='vim -c "/$(date --date="yesterday" +%m\\/%d\\/%Y)" ~/Dropbox/notes/Onething/*.txt'
-alias sch='vim ~/Dropbox/notes/Rutgers/course_schedule.txt'
-alias vihor='vim ~/Dropbox/notes/Horizons_of_Focus/*'
-alias vip="vim -c 'execute \"normal \\<c-p>\" '"
-
-# ---------- SSH / FTP ----------
-alias sftp-rutgers='sftp bmb181@less.cs.rutgers.edu'
-alias ssh-aphrodite='ssh -p 34588 bryan@aphrodite'
-alias ssh-athena="ssh -p $ATHENAS_SSH_PORT bryan@$ATHENAS_DDNS_HOSTNAME"
-alias ssh-artemis="ssh root@67.207.92.152"
-alias ssh-rutgers='ssh bmb181@less.cs.rutgers.edu'
-
-# ---------- Java ----------
+alias CDoc='LangDoc -e c -m'
+alias Doc='LangDoc -e sh -m'
 alias Java='java -classpath .:../bin'
+alias JavaDoc='LangDoc -e java -m'
 alias Javac='javac -d ../bin'
 alias Jdb='jdb -classpath "../bin"'
-
-# ---------- Git ----------
-alias gcignore='git add .gitignore && git commit -m "Update: .gitignore file"'
-alias ggrep='git rev-list --all | xargs git grep -n --break --heading'
-alias ghooks='rm -rf .git/hooks && git init' 
-alias gnlog="git log --oneline --color=always | nl -s ':  ' -v 0 | less"
-alias gpf='git push -f'
-alias gho='ghi open'
-alias grc='git rebase --continue'
-gri() { git rebase -i HEAD~$1; }
-
-# ---------- Overriden Commands ----------
+alias PyDoc='LangDoc -e py -m'
+alias actvenv='source venv/bin/activate'
 alias ag='ag --hidden'
+alias alg='alias | grep -e'
+auto() { nohup autodemo "$@" &> /dev/null & disown && clear; }
 alias cal='cal -n 3 | less'
 alias ccat='pygmentize -g'
 alias cower='cower -c'
-alias dayplan='dayplan -d'
+alias cp="cp -i"
+alias cppinit='cinit ++'
+cprof() { python -m cProfile -s "$@" | less; }
+alias del='sudo pacman -Rs'
+alias delshots='confirm "find /home/bryan/Dropbox/var/aphrodite-motion -name \"*$(date +%Y%m%d)*\" -delete"'
+alias dfs='dropbox-cli filestatus'
+alias dst='dropbox-cli status'
+alias dstart='dropbox-cli start'
+alias dstop='dropbox-cli stop'
+alias du='ncdu --color dark'
+alias gcignore='git add .gitignore && git commit -m "Update: .gitignore file"'
+alias ggrep='git rev-list --all | xargs git grep -n --break --heading'
 alias ghc='stack ghc'
 alias ghci='stack ghci'
+alias gho='ghi open'
+alias ghooks='rm -rf .git/hooks && git init' 
+alias gmutt="neomutt -e 'source ~/.mutt/hooks/bryanbugyi34.gmail'"
+alias gnlog="git log --oneline --color=always | nl -s ':  ' -v 0 | less"
+alias gpf='git push -f'
+alias grc='git rebase --continue'
+gri() { git rebase -i HEAD~$1; }
+alias lim='vim -c "normal \`0" -c "bdelete 1"'
+alias loc='locate -r'
+alias lpass-login='lpass login bryanbugyi34@gmail.com'
 alias matlab='matlab -nojvm -nodisplay -nosplash'
-alias du='ncdu --color dark'
+alias mirror='xrandr --output DVI-I-1-1 --auto --same-as LVDS1'
+mkcd() { mkdir -p $1 && cd $1; }
+alias mkpkg='makepkg -si'
+alias mutt="neomutt"
+alias mv="mv -i"
+alias nomirror='xrandr --output DVI-I-1-1 --auto --right-of LVDS1'
+alias onething='vim -c "/$(date --date="yesterday" +%m\\/%d\\/%Y)" ~/Dropbox/notes/Onething/*.txt'
+alias pdb='ipdb'
+alias psgrep='ps -aux | grep -E'
+pvar() { set | grep -i -e "^$1"; }
+alias rg='ranger'
+rip() { nohup torrent -d "$@" &> /dev/null & disown; }
+alias rm="rm -i"
+alias rmutt='sudo neomutt -f /var/spool/mail/root'
+alias sch='vim ~/Dropbox/notes/Rutgers/course_schedule.txt'
+alias sftp-rutgers='sftp bmb181@less.cs.rutgers.edu'
+alias snapshots='find /home/bryan/Dropbox/var/aphrodite-motion -name "*$(date +%Y%m%d)*" | sort | xargs imv && delshots'
+ss() { tmux send-keys "sleep 1.5 && !-2" "Enter"; }
+alias ssh-aphrodite='ssh -p 34588 bryan@aphrodite'
+alias ssh-artemis="ssh root@67.207.92.152"
+alias ssh-athena="ssh -p $ATHENAS_SSH_PORT bryan@$ATHENAS_DDNS_HOSTNAME"
+alias ssh-rutgers='ssh bmb181@less.cs.rutgers.edu'
 alias sudo='sudo '  # makes aliases visible to sudo
 alias time='/usr/bin/time'
 alias tree='clear && tree -I "venv*|__pycache__*|coverage*"'
-alias loc='locate -r'
-# Remove/Overwrite Files Safely
-alias rm="rm -i"
-alias mv="mv -i"
-alias cp="cp -i"
-
-# ---------- transmission-remote ----------
 tsm() { transmission-remote -l; }
+tsm-add() { transmission-remote -a $1; }
+tsm-boost() { transmission-remote -t$1 -Bh -phall -pr250; }
+tsm-purge() { transmission-remote -t$1 -rad; }
+tsm-rm() { transmission-remote -t$1 -r; }
 tsm-start() { transmission-daemon; }
 tsm-stop() { killall -9 transmission-daemon; }
 tsm-watch() { watch -n 1 transmission-remote -l; }
-tsm-add() { transmission-remote -a $1; }
-tsm-boost() { transmission-remote -t$1 -Bh -phall -pr250; }
-tsm-rm() { transmission-remote -t$1 -r; }
-tsm-purge() { transmission-remote -t$1 -rad; }
-rip() { nohup torrent -d "$@" &> /dev/null & disown; }
-
-# ---------- LangDoc ----------
-alias PyDoc='LangDoc -e py -m'
-alias JavaDoc='LangDoc -e java -m'
-alias CDoc='LangDoc -e c -m'
-alias Doc='LangDoc -e sh -m'
-
-# ---------- Package Management ----------
-alias del='sudo pacman -Rs'
-alias mkpkg='makepkg -si'
-
-# ---------- Python
-alias pdb='ipdb'
-cprof() { python -m cProfile -s "$@" | less; }
+alias vcplug="vim -c ':PluginClean'"
+alias vihor='vim ~/Dropbox/notes/Horizons_of_Focus/*'
+alias vip="vim -c 'execute \"normal \\<c-p>\" '"
+alias vplug="vim -c ':PluginInstall'"
+alias w='which'
+alias watdst='watch -n 5 dropbox-cli status'
