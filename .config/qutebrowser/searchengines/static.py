c1d2be392e4e20aa29963c3e29e01a1b07718558
Bryan Bugyi <bryanbugyi34@gmail.com>
2018-07-27 17:15:00 -0400

    Ref: abstract out google 'site' feature into static function

--- .config/qutebrowser/searchengines/static.py
+++ .config/qutebrowser/searchengines/static.py
@@ -13,19 +13,9 @@ def stackoverflow(n, *, prefix=None):
     Args:
         prefix (opt): If a string is provided for @prefix, it will be appended to the final query.
     """
-    try:
-        assert isinstance(prefix, (str, type(None))), "@prefix arguement must be a string."
-    except AssertionError as e:
-        raise ValueError(str(e))
-
-    if prefix is None:
-        prefix = ''
-    elif prefix[-1] != ' ':
-        prefix = prefix + ' '
-
+    prefix = _validate_prefix(prefix)
     fmt = '{0}{{}} site:stackoverflow.com&source=lnt&tbs=cdr%3A1%2Ccd_min%3A{1}%2F{2}%2F{3}%2Ccd_max%3A&tbm='
     D = _n_years_ago(n)
-
     return google(fmt.format(prefix, D.month, D.day, D.year))
 
 
@@ -33,6 +23,29 @@ def _n_years_ago(n):
     """Return a datetime N years ago."""
     today = dt.date.today()
     return today.replace(year=(today.year - n))
+
+
+def site(*domains, prefix=None):
+    """Returns google search string using Google's advanced 'site' option.
+
+    Args:
+        prefix (opt): static content to prepend to query.
+    """
+    prefix = _validate_prefix(prefix)
+    return google('{0}{{}} {1}'.format(prefix, ' OR '.join(['site:' + S for S in domains])))
+
+
+def _validate_prefix(prefix):
+    """Validates and Beautifies @prefix Argument"""
+    try:
+        assert isinstance(prefix, (str, type(None))), "@prefix arguement must be a string."
+    except AssertionError as e:
+        raise ValueError(str(e))
+
+    if prefix is None:
+        return ''
+    elif prefix[-1] != ' ':
+        return prefix + ' '
 
 
 def google(query):
