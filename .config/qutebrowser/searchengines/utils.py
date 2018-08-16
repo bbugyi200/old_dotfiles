1f4460408ad8f76746d7faeb5772d6c7a2197cb5
Bryan Bugyi <bryanbugyi34@gmail.com>
2018-08-16 15:07:13 -0400

    Fix: spaced can't be encoded as plus symbols

--- .config/qutebrowser/searchengines/utils.py
+++ .config/qutebrowser/searchengines/utils.py
@@ -8,12 +8,13 @@ def encode(query):
 
     A list of HTML URL Codes can be found at: https://www.w3schools.com/tags/ref_urlencode.asp
     """
-    code_map = [('!', '%21'), ('#', '%23'), (' ', '%2B'), (':', '%3A'), ('@', '%40'), ('\\', '%5C')]
+    code_map = [('!', '%21'), ('#', '%23'), (' ', '%20'), (':', '%3A'), ('@', '%40'), ('\\', '%5C')]
 
-    temp = query
+    encoded = query
     for a, b in code_map:
-        temp = temp.replace(a, b)
+        encoded = encoded.replace(a, b)
 
     # preserves python format strings
-    y = re.sub(r'{(\d)%3A', r'{\1:', temp)
-    return y
+    encoded = re.sub(r'{(\d)%3A', r'{\1:', encoded)
+
+    return encoded
