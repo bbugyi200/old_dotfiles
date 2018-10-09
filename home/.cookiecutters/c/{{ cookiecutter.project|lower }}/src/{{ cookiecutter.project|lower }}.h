a7ac107d632c8072edc0a4c56ad07cdc13a9f7d2
Bryan Bugyi <bryanbugyi34@gmail.com>
2018-10-09 12:15:03 -0400

    Update dotfiles

--- home/.cookiecutters/c/{{ cookiecutter.project|lower }}/src/{{ cookiecutter.project|lower }}.h
+++ home/.cookiecutters/c/{{ cookiecutter.project|lower }}/src/{{ cookiecutter.project|lower }}.h
@@ -2,6 +2,7 @@
 #define INCLUDED_{{ cookiecutter.project|upper }}
 
 #include <{{ 'stdio.h' if cookiecutter.filetype == 'c' else 'gutils.h' }}>
+{% if cookiecutter.filetype == 'c' %}#include <stdlib.h>{% endif %}
 {% if cookiecutter.filetype == 'c' %}#include <string.h>{% endif %}
 {% if cookiecutter.filetype == 'c' %}#include <unistd.h>{% endif %}
 
