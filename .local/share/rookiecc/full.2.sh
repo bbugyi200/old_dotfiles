bd3354602d57f512796cbb4e6626ee209a23ac8f
Bryan Bugyi <bryanbugyi34@gmail.com>
2018-11-08 09:47:05 -0500

    Update home

---
+++ .local/share/rookiecc/full.2.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+read -r -d '' doc << EOM
+{% START HERE %}
+EOM
+
+# ---------- Modules ----------
+source /home/bryan/Dropbox/scripts/modules/bash/gutils.sh
+
+# ---------- Command-line Arguments ----------
+eval set -- "$(getopt -o "d,h,v" -l "debug,help,verbose" -- "$@")"
+
+scriptname="${scriptname}"
+usage="${usage}"
+
+# shellcheck disable=SC2154
+read -r -d '' help << EOM
+${usage^}
+
+${doc}
+
+Command-line Options:
+    -d | --debug
+        Enable debug mode.
+
+    -v | --verbose
+        Enable verbose output.
+
+    -h | --help
+        View this help message.
+EOM
+
+while [[ -n "$1" ]]; do
+    case $1 in
+       -d|--debug )
+           debug=true
+           ;;
+       -h|--help )
+           echo "${help}"
+           exit 0
+           ;;
+       -v|--verbose )
+           verbose=true
+           ;;
+       -- )
+           shift
+           break
+           ;;
+    esac
+    shift
+done
+
+if [[ "${debug}" = true && "${verbose}" = true ]]; then
+    PS4='$LINENO: '
+    set -x
+fi
+
+# ---------- Main ----------
